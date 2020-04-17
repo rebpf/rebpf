21b80feaba730208558a7469348a638dbc183096
Simon Chopin <s.chopin@alphalink.fr>
2020-04-17 12:34:53 +0200

    rebpf: helpers: add a wrapper for bpf_redirect_map

--- rebpf/src/helpers.rs
+++ rebpf/src/helpers.rs
@@ -4,7 +4,7 @@
 // (c) Lorenzo Vannucci
 
 use rebpf_sys::libbpf_sys as libbpf;
-use crate::{BpfMapDef, BpfUpdateElemType, error::Error};
+use crate::{BpfMapDef, BpfUpdateElemType, error::Error, xdp::XdpAction};
 use std::{
     os::raw::{c_void, c_int},
     option::Option,
@@ -42,6 +42,34 @@ pub fn bpf_map_update_elem<'a, 'b, T, U>(map: &'a mut BpfMapDef<T, U>, key: &'b 
         return Err(Error::BpfMapUpdateElem(r));
     }
     Ok(())
+}
+
+/// This function is a very thin wrapper around the built-in bpf_redirect_map.
+///
+/// See the [kernel documentation](https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/include/uapi/linux/bpf.h#n1626)
+/// for more details.
+pub fn bpf_redirect_map<'a, 'b, U>(
+    map: &'a BpfMapDef<u32, U>,
+    key: &'b u32,
+    default_action: XdpAction,
+) -> XdpAction {
+    let flags: u32 = default_action as u32;
+    if flags > XdpAction::TX as u32 {
+        // We panic here because this is a programming error, not something
+        // that would occur during a normal execution.
+        panic!("The default action must be one of ABORTED, DROP, PASS or TX");
+    }
+
+    type FPtrType = extern "C" fn(m: *const c_void, k: *const c_void, f: u64) -> c_int;
+    unsafe {
+        let f: FPtrType = mem::transmute(libbpf::bpf_func_id_BPF_FUNC_redirect_map as usize);
+        let r = f(
+            to_const_c_void(&map.map_def),
+            to_const_c_void(key),
+            flags as u64,
+        );
+        mem::transmute::<i32, XdpAction>(r)
+    }
 }
 
 pub(crate) fn to_const_c_void<T>(v: &T) -> *const c_void {
