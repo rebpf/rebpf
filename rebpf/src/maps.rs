aa063cbe419efa719bb0a09a98250a7ccad9091e
Simon Chopin <s.chopin@alphalink.fr>
2020-04-27 19:47:40 +0200

    maps: rework the traits and macros for better type safety
    This patch has actually two components, that are so intertwined that
    it wasn't feasible to split them.
    
    The new `map_def` macro is there to completely wrap the map wrapper
    definition in a sensible way. To be honest, the reason it got started
    was simply that rustfmt systematically removed the empty '<>' in the
    macros, causing compilation failures.
    
    The new format allows us to semantically use the generic types as key
    and value types, which was very useful for the second component.
    
    The type of the content of a map are supposed to be built into the type
    of the map itself. Yet, the previous macros tended towards repeating
    some of those types when generating the implementations for the lookup
    trait. As I'm working on adding an Update trait with similar signatures,
    this could very easily be a source of mistakes. Via the new Map trait,
    we can now easily deduce the Key and Value types when we build the map
    type, and use them directly in the implementation macros without having
    to specify them again :-).

--- rebpf/src/maps.rs
+++ rebpf/src/maps.rs
@@ -1,9 +1,15 @@
 /// This crate contains traits defining common operations on BPF maps that can
 /// be executed both on the userspace side and the BPF side.
 
-pub trait Lookup {
+/// This trait is implemented by all the map wrapper types, as
+/// as convenient way to communicate their underlying types to the
+/// Rust type system.
+pub trait Map {
     type Key;
     type Value;
+}
+
+pub trait Lookup: Map {
     /// Lookup the map content associated with the given key.
     ///
     /// Note that the return value is a mere copy of said content.
