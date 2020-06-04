afe0cf9a078ae8d113315ad0daf9ff52cff76b52
Lorenzo Vannucci <lorenzo@vannucci.io>
2020-06-04 03:28:00 +0200

    add import_panic_symbol macro

--- rebpf-macro/src/lib.rs
+++ rebpf-macro/src/lib.rs
@@ -6,9 +6,9 @@
 extern crate proc_macro;
 
 use proc_macro::{TokenStream, TokenTree};
+use proc_macro2::Span;
 use quote::quote;
 use syn::Error;
-use proc_macro2::Span;
 
 #[proc_macro_attribute]
 pub fn sec(args: TokenStream, input: TokenStream) -> TokenStream {
@@ -19,17 +19,42 @@ pub fn sec(args: TokenStream, input: TokenStream) -> TokenStream {
         } else {
             let err = Error::new(
                 Span::call_site(),
-                "Error occured parsing attribute args: must be a string, i.e.:\n seq(\"my_function\")"
+                "Error occured parsing sec attribute args: must be a string, i.e.:\n seq(\"my_function\")"
             ).to_compile_error();
             return TokenStream::from(err);
         }
     }
     link_name.retain(|c| c != '\"');
-    
+
     let mut tts: TokenStream = TokenStream::new();
     let attr = quote! { #[no_mangle] #[link_section = #link_name] };
     let tts_attr = TokenStream::from(attr);
     tts.extend(tts_attr);
     tts.extend(input);
+    tts
+}
+
+#[proc_macro]
+pub fn import_panic_symbol(input: TokenStream) -> TokenStream {
+    if input.is_empty() == false {
+        let err = Error::new(
+            Span::call_site(),
+            "Error occured parsing import_panic_symbol parameters: must be empty",
+        )
+        .to_compile_error();
+        return TokenStream::from(err);
+    }
+    let mut tts: TokenStream = TokenStream::new();
+    let attr = quote! {
+        #[no_mangle]
+        pub fn _ZN4core9panicking5panic17h0fd4184f909d9498E() {
+        }
+
+        #[no_mangle]
+        pub fn _ZN4core9panicking18panic_bounds_check17h657fd036b4404d7aE() {
+        }
+    };
+    let tts_attr = TokenStream::from(attr);
+    tts.extend(tts_attr);
     tts
 }
