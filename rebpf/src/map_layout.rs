af3dd90d0ade820fcc509baa8f3b6c663a61e608
Simon Chopin <chopin.simon@gmail.com>
2020-08-30 23:57:18 +0200

    map_layout: implement AsMut for the value wrapper
    This makes it easier to modify the content of a buffer.

--- rebpf/src/map_layout.rs
+++ rebpf/src/map_layout.rs
@@ -120,6 +120,12 @@ impl<T> AsRef<T> for PerCpuValue<T> {
     }
 }
 
+impl<T> AsMut<T> for PerCpuValue<T> {
+    fn as_mut(&mut self) -> &mut T {
+        &mut self.0
+    }
+}
+
 /// Memory layout matching per-CPU values, with its specificities.
 /// Notably, the kernel rounds up the size of an individual value to a multiple
 /// of 8, which means we cannot use a simple packed layout as in a Vec.
