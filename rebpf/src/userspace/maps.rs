6cc1f42a2b041e2bca764d3f20161bf0c505b9d0
Lorenzo Vannucci <lorenzo@vannucci.io>
2020-09-19 19:40:57 +0200

    restyle maps macro using duplicate crates

--- rebpf/src/userspace/maps.rs
+++ rebpf/src/userspace/maps.rs
@@ -47,18 +47,6 @@ pub trait Lookup: Map {
     }
 }
 
-macro_rules! map_impl {
-    ($type_const:expr) => {
-        pub fn from_obj(bpf_obj: &BpfObject, map_name: &str) -> Result<Self> {
-            let fd = extract_map_fd(bpf_obj, map_name)?;
-            Ok(Self { fd })
-        }
-        pub fn extract_info(&self) -> Result<BpfMapInfo> {
-            extract_checked_info(&self.fd, $type_const)
-        }
-    };
-}
-
 fn extract_map_fd<K, V, L: MapLayout<V>>(
     bpf_obj: &BpfObject,
     map_name: &str,
@@ -80,38 +68,13 @@ fn extract_checked_info<K, V, L: MapLayout<V>>(
         ))
     }
 }
-/*
-#[duplicate(
-    [
-        map_type    [ CpuMap ]
-        key         [ u32 ]
-        value       [ u32 ]
-        map_layout  [ ScalarLayout ]
-    ]
-)]
-pub struct map_type {
-    fd: BpfMapFd<key, value, map_layout>,
-}
-*/
-//  [Array<T>]  [u32]  [T]  [ScalarLayout]
-
-
-// #[duplicate(
-//   map_type            key      value    map_layout;     
-//   [ CpuMap ]          [ u32 ]  [ u32 ]  [ ScalarLayout ];
-//   [ Array<T> ]        [ u32 ]  [ T ]    [ ScalarLayout ];
-//   [ PerCpuArray<T> ]  [ u32 ]  [ T ]    [ PerCpuLayout ];
-// )]
-// pub struct map_type {
-//     fd: super::BpfMapFd<key, value, super::map_layout>,
-// }
 
 duplicate_inline!{
 [
   map_type            generics  key      value    layout            type_const;     
-  [ CpuMap ]          [ ]       [ u32 ]  [ u32 ]  [ ScalarLayout ]  [ ];
-  [ Array ]           [ T ]     [ u32 ]  [ T ]    [ ScalarLayout ]  [ ];
-  [ PerCpuArray ]     [ T ]     [ u32 ]  [ T ]    [ PerCpuLayout ]  [ ];
+  [ CpuMap ]          [ ]       [ u32 ]  [ u32 ]  [ ScalarLayout ]  [ BpfMapType::CPUMAP ];
+  [ Array ]           [ T ]     [ u32 ]  [ T ]    [ ScalarLayout ]  [ BpfMapType::ARRAY ];
+  [ PerCpuArray ]     [ T ]     [ u32 ]  [ T ]    [ PerCpuLayout ]  [ BpfMapType::PERCPU_ARRAY ];
 ]
     pub struct map_type<generics> {
         fd: BpfMapFd<key, value, layout>,
@@ -128,83 +91,9 @@ duplicate_inline!{
             Ok(Self { fd })
         }
         pub fn extract_info(&self) -> Result<BpfMapInfo> {
-            extract_checked_info(&self.fd, $type_const)
+            extract_checked_info(&self.fd, type_const)
         }
     }
+    impl<generics> Update for map_type<generics> {}
+    impl<generics> Lookup for map_type<generics> {}
 }
-
-
-
-
-macro_rules! map_def {
-    ($(#[$outer:meta])*
-    struct $map_type:ident < $key:ty, $value:ty, $layout:ty > : $type_const:expr) => {
-        #[repr(transparent)]
-        $(#[$outer])*
-        pub struct $map_type<$key, $value> {
-            fd: BpfMapFd<$key, $value, $layout>,
-        }
-        impl<$key, $value> $map_type<$key, $value> { map_impl! {$map_type<$key, $value>: $type_const} }
-        impl<$Key, $value> Map for $map_type<$key, $value> {
-            type Key = $key;
-            type Value = $value;
-            type Layout = $layout;
-            fn fd(&self) -> &BpfMapFd<$key, $value, $layout> {&self.fd}
-        }
-    };
-    ($(#[$outer:meta])*
-    struct $map_type:ident < $value:ident, $layout:ty > : $type_const:expr) => {
-        #[repr(transparent)]
-        $(#[$outer])*
-        pub struct $map_type <$value> {
-            fd: BpfMapFd<u32, $value, $layout>,
-        }
-        impl<$value> $map_type<$value> { map_impl!($type_const); }
-        impl<$value> Map for $map_type<$value> {
-            type Key = u32;
-            type Value = $value;
-            type Layout = $layout;
-            fn fd(&self) -> &BpfMapFd<u32, $value, $layout> {&self.fd}
-        }
-    };
-    ($(#[$outer:meta])*
-    struct $map_type:ident < $layout:ty > : $type_const:expr) => {
-        #[repr(transparent)]
-        $(#[$outer])*
-        pub struct $map_type {
-            fd: BpfMapFd<u32, u32, $layout>,
-        }
-        impl $map_type { map_impl!($type_const); }
-        impl Map for $map_type {
-            type Key = u32;
-            type Value = u32;
-            type Layout = $layout;
-            fn fd(&self) -> &BpfMapFd<u32, u32, $layout> {&self.fd}
-        }
-    };
-}
-
-macro_rules! impl_update_gen {
-    () => {
-        fn update(
-            &mut self,
-            key: &Self::Key,
-            value: &Self::Value,
-            flags: BpfUpdateElemFlags,
-        ) -> Result<()> {
-            libbpf::bpf_map_update_elem(&self.fd, key, value, flags)
-        }
-    };
-}
-
-//map_def!(struct CpuMap <ScalarLayout> : BpfMapType::CPUMAP);
-//impl Update for CpuMap {}
-//impl Lookup for CpuMap {}
-
-//map_def!(struct Array<T, ScalarLayout>: BpfMapType::ARRAY);
-//impl<T> Update for Array<T> {}
-//impl<T> Lookup for Array<T> {}
-
-// map_def!(struct PerCpuArray<T, PerCpuLayout>: BpfMapType::PERCPU_ARRAY);
-// impl<T> Update for PerCpuArray<T> {}
-// impl<T> Lookup for PerCpuArray<T> {}
