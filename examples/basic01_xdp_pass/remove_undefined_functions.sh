378c7a2c264436cd0bb33aebfba4b80335294949
Trinity Pointard <trinity.pointard@gmail.com>
2021-04-01 00:53:01 +0200

    various improvements on examples build scripts
    - make rustc emite llvm-ir instead of bc that get dissassembled. It seems
    like an unneccessay step and I had issue with llc not supporting
    llvm-dis output (invalid token "blockcount")
    - make `remove_undefined_functions.sh` more generic by deleting calls to
    diverging functions taking an `std::panic::Location`. The symbols
    slice_start_index_len_fail and slice_end_index_len_fail were not removed
    from packet_parser example, and where causing bpf verifier issues for me.
    - add a gitignore to not track build artifacts

--- examples/basic01_xdp_pass/remove_undefined_functions.sh
+++ examples/basic01_xdp_pass/remove_undefined_functions.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 INPUT_OBJ=$1
 OUTPUT_OBJ=$2
-cat $INPUT_OBJ | sed -E "s/(.*)call(.*)slice_index_len_fail(.*)/  ret i32 0/" | sed -E "s/(.*)call(.*)slice_index_order_fail(.*)/  ret i32 0/" | sed -E "s/(.*)call(.*)panic_bounds_check(.*)/  ret i32 0/" | sed -E "s/unreachable//" > $OUTPUT_OBJ
+cat $INPUT_OBJ | sed -zE "s/\n([^;\n]*)call([^\n]*)std::panic::Location([^\n]*)\n  unreachable\n/\n  ret i32 0\n/g" > $OUTPUT_OBJ
--- examples/packet_parser/build.sh
+++ examples/packet_parser/build.sh
@@ -2,10 +2,8 @@
 BPF_PROG_NAME="kern"
 USER_PROG_NAME="user"
 OUT_DIR="ebpf_output"
-LLVM_DIS=llvm-dis-9
 LLC_OPTIONS="-march=bpf -filetype=obj"
-LLC=llc-9
-OUT_BC=$OUT_DIR"/"$BPF_PROG_NAME".bc"
+LLC=llc
 OUT_IR_TEMP=$OUT_DIR"/"$BPF_PROG_NAME"_temp.ll"
 OUT_IR=$OUT_DIR"/"$BPF_PROG_NAME".ll"
 OUT_ELF=$OUT_DIR"/"$BPF_PROG_NAME".o"
@@ -13,13 +11,12 @@ RM_UND=./remove_undefined_functions.sh
 
 mkdir -p $OUT_DIR
 TARGET_DIRECTORY="target/release/deps"
-TARGET=$(echo $TARGET_DIRECTORY"/kern*.bc")
+TARGET=$(echo $TARGET_DIRECTORY"/kern*.ll")
 if [ -d $TARGET_DIRECTORY ]; then
     rm -f $TARGET
 fi
-cargo rustc --lib --release -- --emit=llvm-bc
-cp $TARGET $OUT_BC
-$LLVM_DIS $OUT_BC -o $OUT_IR_TEMP
+cargo rustc --lib --release -- --emit=llvm-ir
+cp $TARGET $OUT_IR_TEMP
 $RM_UND $OUT_IR_TEMP $OUT_IR
 $LLC $OUT_IR $LLC_OPTIONS -o $OUT_ELF 
 #llvm-objcopy $OUT_ELF --remove-section=.text
